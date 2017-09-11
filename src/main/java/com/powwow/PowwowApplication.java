486744edaeda56283247afc1e208bc0849900ea6
dasamantaraoaditya <dasamantarao.aditya.com>
2017-09-12 00:46:22 +0530

    initial commit for a setup project

---
+++ src/main/java/com/powwow/PowwowApplication.java
@@ -0,0 +1,12 @@
+package com.powwow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowwowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowwowApplication.class, args);
+	}
+}
